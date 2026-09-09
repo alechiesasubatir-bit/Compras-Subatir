23655235c281e00f56212f2e71ea3a55579f8ff6
Alejandro Chiesa <achiesa77@gmail.com>
2026-09-08 22:33:30 -0300

    Version

--- deposito/sw.js
+++ deposito/sw.js
@@ -25,7 +25,7 @@
 //  cambiar el archivo, el navegador instala el worker nuevo y
 //  descarta el cache viejo.
 // ============================================================
-self.APP_VER = '2026-09-08.2119';
+self.APP_VER = '2026-09-08.2233';
 var CACHE = 'deposito-' + self.APP_VER;
 
 // Lo mínimo para que la app abra sin señal.

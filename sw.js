32bbd4120edfa937e219676653438a1275ec4639
Alejandro Chiesa <achiesa77@gmail.com>
2026-09-08 21:19:03 -0300

    Version

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
 //  cambiar el archivo, el navegador instala el worker nuevo y
 //  descarta el cache viejo.
 // ============================================================
-self.APP_VER = '2026-09-08.2109';
+self.APP_VER = '2026-09-08.2119';
 var CACHE = 'compras-' + self.APP_VER;
 
 // Lo mínimo para que la app abra sin señal.
